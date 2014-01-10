3ec3bb39195950481e8e689bd29a0d7b360bff11
thks10 <softinus@users.noreply.github.com>
2014-01-10 08:43:31 +0000

    -카메라뷰를 동적할당으로 변경함.

--- src/com/raimsoft/camera/CameraView.java
+++ src/com/raimsoft/camera/CameraView.java
@@ -18,6 +18,7 @@ class CameraView extends SurfaceView implements SurfaceHolder.Callback
     Camera mCamera;
     
     static boolean bAlreadyCreated= false;
+    boolean bFrontCamera= false;
     
     public CameraView(Context context)
     {
@@ -28,6 +29,19 @@ class CameraView extends SurfaceView implements SurfaceHolder.Callback
         mHolder = getHolder();
         mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+    
+    public CameraView(Context context, boolean _bFrontCamera)
+    {
+        super(context);
+        
+        // SurfaceHolder.Callback을 설정함으로써 Surface가 생성/소멸되었음을
+        // 알 수 있습니다.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        
+        this.bFrontCamera= _bFrontCamera;
     }
 
     /**
@@ -56,7 +70,7 @@ class CameraView extends SurfaceView implements SurfaceHolder.Callback
         mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	}
-
+	
 	public void surfaceCreated(SurfaceHolder holder)
     {
 //		if(bAlreadyCreated)
@@ -66,7 +80,18 @@ class CameraView extends SurfaceView implements SurfaceHolder.Callback
 		
         // Surface가 생성되었다면, 카메라의 인스턴스를 받아온 후 카메라의
         // Preview 를 표시할 위치를 설정합니다.
-    	mCamera= openBackFacingCameraGingerbread();
+    	
+		if(bFrontCamera)	// 앞뒤구분..
+		{
+			mCamera= openFrontFacingCameraGingerbread();
+		}
+		else
+		{
+			mCamera= openBackFacingCameraGingerbread();			
+		}
+		
+    	
+    	
     	mCamera.setDisplayOrientation(90);
     	
         try
@@ -108,8 +133,9 @@ class CameraView extends SurfaceView implements SurfaceHolder.Callback
         // 표시할 영역의 크기를 알았으므로 해당 크기로 Preview를 시작합니다.
         Camera.Parameters parameters = mCamera.getParameters();
         //parameters.set("orientation", "portrait");
-        parameters.setPreviewSize(480, 800);
-        mCamera.setParameters(parameters);
+        //parameters.setPreviewSize(480, 800);
+        //parameters.setPreviewSize(1080, 1920);
+        //mCamera.setParameters(parameters);
         mCamera.startPreview();
     }
     
