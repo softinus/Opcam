6294a87f356a56f478fa579e6266379dd734f69f
thks10 <thks10@4c625e73-aac3-4376-b224-836a11a34f09>
2014-01-12 13:01:56 +0000

    -카메라 콜백 추가. -이미지뷰를 활용한 2 이미지 동시 저장.

--- trunk/src/com/raimsoft/camera/CameraView.java
+++ trunk/src/com/raimsoft/camera/CameraView.java
@@ -124,8 +124,9 @@ class CameraView extends SurfaceView implements SurfaceHolder.Callback
         // 다른 화면으로 돌아가면, Surface가 소멸됩니다. 따라서 카메라의 Preview도 
         // 중지해야 합니다. 카메라는 공유할 수 있는 자원이 아니기에, 사용하지 않을
         // 경우 -액티비티가 일시정지 상태가 된 경우 등 - 자원을 반환해야합니다.
-        mCamera.stopPreview();
-        mCamera = null;
+        //mCamera.stopPreview();
+        //mCamera = null;
+    	this.ReleaseCamera();
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h)
