bdbb720873ca78db908e5304956616333f31063e
thks10 <softinus@users.noreply.github.com>
2014-01-12 13:01:56 +0000

    -카메라 콜백 추가. -이미지뷰를 활용한 2 이미지 동시 저장.

--- src/com/raimsoft/camera/CameraPreview.java
+++ src/com/raimsoft/camera/CameraPreview.java
@@ -1,9 +1,12 @@
 package com.raimsoft.camera;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -17,11 +20,16 @@ public class CameraPreview extends Activity implements OnClickListener
 	private LinearLayout mLayout;
     private CameraView mCView1;
     private CameraView mCView2;
-    private ImageView mImgPreview;
+    private Bitmap mBitmap1;
+    private Bitmap mBitmap2;
+    private ImageView mImgPreview1;
+    private ImageView mImgPreview2;
     
     private Button ShutButton;
-	private PictureCallback mPicture;
+	private PictureCallback mPicture1;
+	private PictureCallback mPicture2;
 	
+	private Context mContext;
 //    private boolean bBackDone= false;
 //    private boolean bFrontDone= false;
     
@@ -29,6 +37,56 @@ public class CameraPreview extends Activity implements OnClickListener
 	protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        
+        mContext= this.getApplicationContext();
+        
+        
+        mPicture1= new PictureCallback()
+        {
+			@Override
+			public void onPictureTaken(byte[] data, Camera camera)
+			{					
+				// 뒤 카메라 메모리를 제거한다.
+				mCView1.ReleaseCamera();
+				mLayout.removeAllViews();
+				
+				// 이미지를 세팅한다.
+				mImgPreview1.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 0.5f));        
+		        mLayout.addView(mImgPreview1);
+		        
+		        mBitmap1 = BitmapFactory.decodeByteArray(data, 0, data.length);
+				mImgPreview1.setImageBitmap(mBitmap1);				
+				
+				// 두번째 앞 카메라를 연다. 
+		        mCView2= new CameraView(mContext, true);
+		        mCView2.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 0.5f));        
+		        mLayout.addView(mCView2);
+			}
+    	};
+    	
+    	mPicture2= new PictureCallback()
+        {
+			@Override
+			public void onPictureTaken(byte[] data, Camera camera) 
+			{	
+				// 앞 카메라 메모리를 제거한다.
+				mCView2.ReleaseCamera();
+				//mLayout.removeAllViews();
+				mLayout.removeView(mCView2);
+				
+				// 이미지를 세팅한다.
+//				mImgPreview1.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 0.5f));
+//				Bitmap bitmap1 = BitmapFactory.decodeByteArray(mData1, 0, mData1.length);
+//				mImgPreview1.setImageBitmap(bitmap1);	
+//				mLayout.addView(mImgPreview1);
+				
+				// 이미지를 세팅한다.
+				mImgPreview2.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 0.5f));
+				mBitmap2= BitmapFactory.decodeByteArray(data, 0, data.length);
+				mImgPreview2.setImageBitmap(mBitmap2);				        
+		        mLayout.addView(mImgPreview2);
+			}
+    	};
         
         // Hide the window title.
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -41,14 +99,20 @@ public class CameraPreview extends Activity implements OnClickListener
         mLayout = (LinearLayout) findViewById(R.id.camera_layout);
         //mCView= (CameraView) findViewById(R.id.camera_preview1);
         
-        mCView1= new CameraView(this, false);        
+        mImgPreview1= new ImageView(this);
+        mImgPreview2= new ImageView(this);
+        
+        mCView1= new CameraView(this, false);
         mCView1.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 0.5f));        
         mLayout.addView(mCView1);
+        
         
         ShutButton= (Button) findViewById(R.id.btn_click);        
         ShutButton.setOnClickListener(this);
         ShutButton.setText("YOU");
     }
+    
+    
 
 	@Override
 	public void onClick(View v)
@@ -57,19 +121,12 @@ public class CameraPreview extends Activity implements OnClickListener
 		{
 			if(ShutButton.getText().equals("YOU"))
 			{
-				mCView1.mCamera.takePicture(null, null, mPicture);
-				//bBackDone= true;
-				
-
-		//        aq.id(R.id.btn_click)
-				 ShutButton.setText("ME");
-				
-				//cameraView.ReleaseCamera();
-				SystemClock.sleep(1000);
+				mCView1.mCamera.takePicture(null, null, mPicture1);
+				ShutButton.setText("ME");
 			}
 			else if(ShutButton.getText().equals("ME"))
-			{
-				//mCView.mCamera.takePicture(null, null, mPicture);
+			{	
+				mCView2.mCamera.takePicture(null, null, mPicture2);
 				ShutButton.setText("-->");
 			}
 			else
