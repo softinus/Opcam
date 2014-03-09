24abde53e61d2da150f66ef306e0020a97186467
thks10 <softinus@users.noreply.github.com>
2014-03-09 14:12:49 +0000

    -gallery 다른 gridview로 대체. -camera에서 button들 크기 dp 수정.

--- src/us/opcam/camera/activity/GalleryActivity.java
+++ src/us/opcam/camera/activity/GalleryActivity.java
@@ -4,16 +4,24 @@ import java.io.File;
 import java.util.ArrayList;
 
 import us.opcam.camera.R;
-import us.opcam.camera.util.GalleryAdapter;
+import us.opcam.camera.util.GridViewAdapter;
+import us.opcam.camera.util.ImageItem;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
-
-import com.origamilabs.library.views.StaggeredGridView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class GalleryActivity extends Activity 
 {
+	private GridView gridView;
+	private GridViewAdapter customGridAdapter;
 	
 
 	@Override
@@ -22,27 +30,19 @@ public class GalleryActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gallery);
 		
-		StaggeredGridView gridView = (StaggeredGridView) this.findViewById(R.id.staggeredGridView1);
-		
-		int margin = getResources().getDimensionPixelSize(R.dimen.grid_margin);		
-		gridView.setItemMargin(margin); // set the GridView margin		
-		gridView.setPadding(margin, 0, margin, 0); // have the margin on the sides as well 
-		
-		File dir=new File(Environment.getExternalStorageDirectory(),"/opcam");  
-        int count=dir.list().length;   
-        String[] fileNames = dir.list();
-        ArrayList<String> arrStrPath= new ArrayList<String>();
-        
-        for(String name : fileNames)
-        {
-        	arrStrPath.add( "/sdcard/opcam/" + name );	// ¿Ã∏ß ∏∏µÍ
-        }
-        
-        String[] filePaths= arrStrPath.toArray(new String[arrStrPath.size()]);        
-		GalleryAdapter adapter = new GalleryAdapter(GalleryActivity.this, R.id.imageView1, filePaths);
-		
-		gridView.setAdapter(adapter);
-		adapter.notifyDataSetChanged();
+		gridView = (GridView) findViewById(R.id.gridView);
+		customGridAdapter = new GridViewAdapter(this, R.layout.row_grid, getData());
+		gridView.setAdapter(customGridAdapter);
+
+		gridView.setOnItemClickListener(new OnItemClickListener()
+		{
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+			{
+				Toast.makeText(GalleryActivity.this, position + "#Selected",
+						Toast.LENGTH_SHORT).show();
+			}
+
+		});
 	}
 
 	@Override
@@ -52,5 +52,33 @@ public class GalleryActivity extends Activity
 		getMenuInflater().inflate(R.menu.gallery, menu);
 		return true;
 	}
+	
+	private ArrayList<ImageItem> getData()
+	{
+		final ArrayList<ImageItem> imageItems = new ArrayList<ImageItem>();
+		
+		File dir=new File(Environment.getExternalStorageDirectory(), "/opcam");  
+		int count= dir.list().length;   
+		String[] fileNames = dir.list();
+		
+		for(String name : fileNames)
+		{
+			String strURI= "/sdcard/opcam/" + name;	// ¿Ã∏ß ∏∏µÍ
+			final BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inSampleSize = 8;
+			Bitmap bmp = BitmapFactory.decodeFile(strURI, options);
+			
+			if(bmp == null)
+					toast("abnormal path. [" + strURI +"]");
+			else
+				imageItems.add(new ImageItem(bmp, name));
+		}
+		return imageItems;	
+	}
+	
+	public void toast(String msg)
+    {
+        Toast.makeText (this.getApplicationContext(), msg, Toast.LENGTH_SHORT).show ();
+    }
 
 }
