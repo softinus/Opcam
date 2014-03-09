ead6e9fde2481cde37f392ae4daa9e50694ca9aa
thks10 <thks10@4c625e73-aac3-4376-b224-836a11a34f09>
2014-03-09 15:51:25 +0000

    -로딩 글 삭제. -share activity 에 aviary로부터 사진 URI 받아서 띄움.

--- trunk/src/us/opcam/camera/activity/SNSShareActivity.java
+++ trunk/src/us/opcam/camera/activity/SNSShareActivity.java
@@ -2,22 +2,54 @@ package us.opcam.camera.activity;
 
 import us.opcam.camera.R;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
 
-public class SNSShareActivity extends Activity {
+public class SNSShareActivity extends Activity
+{
+	ImageView IMG_share;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_snsshare);
+		
+		IMG_share= (ImageView) findViewById(R.id.img_for_share);
+		
+		Intent intent = getIntent();//인텐트  받아오고
+		
+		Bundle extra= intent.getExtras();
+		Uri imgPath= extra.getParcelable("path");
+		
+		Log.d("SNSShareActivity::onActivityResult", imgPath.toString());
+		
+		IMG_share.setImageURI(imgPath);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.snsshare, menu);
 		return true;
 	}
+
+//	@Override
+//	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+//	{
+//		super.onActivityResult(requestCode, resultCode, data);
+//		
+//		Bundle extra= data.getExtras();
+//		Uri imgPath= extra.getParcelable("path");
+//		
+//		Log.d("SNSShareActivity::onActivityResult", imgPath.toString());
+//		
+//		IMG_share.setImageURI(imgPath);
+//	}
 
 }
