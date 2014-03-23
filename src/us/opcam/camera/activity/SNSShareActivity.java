ced940aecda956d9ed9ab6f3d116ccbd2f2d16ce
thks10 <softinus@users.noreply.github.com>
2014-03-23 05:01:02 +0000

    -Aquery 사용한 facebook login.

--- src/us/opcam/camera/activity/SNSShareActivity.java
+++ src/us/opcam/camera/activity/SNSShareActivity.java
@@ -1,6 +1,9 @@
 package us.opcam.camera.activity;
 
+import org.json.JSONObject;
+
 import us.opcam.camera.R;
+
 import us.opcam.camera.util.StoryLink;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -8,15 +11,21 @@ import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.androidquery.AQuery;
+import com.androidquery.auth.FacebookHandle;
 
 public class SNSShareActivity extends Activity
 {
 	Uri mImagePath;
 	ImageView IMG_share;
+	
+	private FacebookHandle handle;
+	private final int ACTIVITY_SSO= 1000;
+	//private static String PERMISSIONS = "read_stream,read_friendlists,manage_friendlists,manage_notifications,publish_stream,publish_checkins,offline_access,user_photos,user_likes,user_groups,friends_photos";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -60,6 +69,16 @@ public class SNSShareActivity extends Activity
 		}
 		
 		storyLink.openStoryLinkImageApp(this, mImagePath.toString());
+	}
+	
+	public void ShareToFacebook(View v) throws NameNotFoundException
+	{
+		handle= new FacebookHandle(this, "193443224183217", "publish_stream");
+		
+		AQuery aq= new AQuery(v);
+		String url= "https://graph.facebook.com/me/feed";
+		aq.auth(handle).progress(R.id.ProgressBar).ajax(url, JSONObject.class, this, "facebookCb");
+		
 	}
 	
 
